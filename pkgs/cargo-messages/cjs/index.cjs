65615709340920053a0cacbc0bd9ea8921dea8d8
David Herman <david.herman@gmail.com>
2023-05-20 16:45:20 -0700

    try using "moduleResolution": "bundler"

--- pkgs/cargo-messages/cjs/index.cjs
+++ pkgs/cargo-messages/cjs/index.cjs
@@ -1,5 +1,18 @@
 const { currentTarget } = require('@neon-rs/load');
 
+/*
+const TABLE = {
+  'darwin-x64': () => require('@cargo-messages/darwin-x64'),
+  'win32-x64-msvc': () => require('@cargo-messages/win32-x64-msvc'),
+  'aarch64-pc-windows-msvc': () => require('@cargo-messages/win32-arm64-msvc'),
+  'darwin-x64': () => require('@cargo-messages/darwin-x64'),
+  'darwin-arm64': () => require('@cargo-messages/darwin-arm64'),
+  'linux-x64-gnu': () => require('@cargo-messages/linux-x64-gnu'),
+  'linux-arm-gnueabihf': () => require('@cargo-messages/linux-arm-gnueabihf'),
+  'android-arm-eabi': () => require('@cargo-messages/android-arm-eabi')
+};
+*/
+
 let saved = null;
 
 function addon() {
--- pkgs/cli/src/index.ts
+++ pkgs/cli/src/index.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+import commandLineCommands from 'command-line-commands';
+import { printErrorWithUsage, printError, printMainUsage } from './print.js';
+import { Command, CommandName, CommandClass, asCommandName, commandFor } from './command.js';
+/*
 import { createRequire } from 'node:module';
 
 const absoluteRequire = createRequire(import.meta.url);
@@ -7,10 +11,7 @@ const absoluteRequire = createRequire(import.meta.url);
 global['require'] = function(spec: string) {
   return absoluteRequire(spec);
 } as any;
-
-import commandLineCommands from 'command-line-commands';
-import { printErrorWithUsage, printError, printMainUsage } from './print.js';
-import { Command, CommandName, CommandClass, asCommandName, commandFor } from './command.js';
+*/
 
 class Cli {
   parse(): Command {
