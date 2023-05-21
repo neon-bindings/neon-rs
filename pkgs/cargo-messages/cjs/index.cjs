626f43b73dca0d6ce09dc2dfa3f6ec97392390a0
David Herman <david.herman@gmail.com>
2023-05-20 23:34:02 -0700

    abstract lazy loading, and make sure to late bind

--- pkgs/cargo-messages/cjs/index.cjs
+++ pkgs/cargo-messages/cjs/index.cjs
@@ -1,9 +1,4 @@
-const {
-  fromFile,
-  fromStdin,
-  findArtifact,
-  findFileByCrateType
-} = require('./load.cjs');
+const addon = require('./load.cjs');
 
 const PRIVATE = {};
 
@@ -20,7 +15,7 @@ class CargoArtifact {
   }
 
   findFileByCrateType(crateType) {
-    return findFileByCrateType(this._kernel, crateType);
+    return addon.findFileByCrateType(this._kernel, crateType);
   }
 }
 
@@ -30,12 +25,12 @@ class CargoMessages {
     this._mount = options.mount || null;
     this._manifestPath = options.manifestPath || null;
     this._kernel = options.file
-      ? fromFile(options.file, this._mount, this._manifestPath)
-      : fromStdin(this._mount, this._manifestPath);
+      ? addon.fromFile(options.file, this._mount, this._manifestPath)
+      : addon.fromStdin(this._mount, this._manifestPath);
   }
 
   findArtifact(crateName) {
-    const found = findArtifact(this._kernel, crateName);
+    const found = addon.findArtifact(this._kernel, crateName);
     return found
       ? new CargoArtifact(PRIVATE, found)
       : null;
--- pkgs/cargo-messages/cjs/load.cjs
+++ pkgs/cargo-messages/cjs/load.cjs
@@ -1,6 +1,30 @@
 const { currentTarget } = require('@neon-rs/load');
 
-const TABLE = {
+function lazy(loaders, exports) {
+  let loaded = null;
+
+  function load() {
+    if (loaded) {
+      return loaded;
+    }
+    const target = currentTarget();
+    if (!loaders.hasOwnProperty(target)) {
+      throw new Error(`no precompiled module found for ${target}`);
+    }
+    loaded = loaders[target]();
+    return loaded;
+  }
+
+  let module = {};
+
+  for (const key of exports) {
+    Object.defineProperty(module, key, { get() { return load()[key]; } });
+  }
+
+  return module;
+}
+
+module.exports = lazy({
   'darwin-x64': () => require('@cargo-messages/darwin-x64'),
   'win32-x64-msvc': () => require('@cargo-messages/win32-x64-msvc'),
   'aarch64-pc-windows-msvc': () => require('@cargo-messages/win32-arm64-msvc'),
@@ -9,25 +33,9 @@ const TABLE = {
   'linux-x64-gnu': () => require('@cargo-messages/linux-x64-gnu'),
   'linux-arm-gnueabihf': () => require('@cargo-messages/linux-arm-gnueabihf'),
   'android-arm-eabi': () => require('@cargo-messages/android-arm-eabi')
-};
-
-let loaded = null;
-
-function load() {
-  if (loaded) {
-    return loaded;
-  }
-  const target = currentTarget();
-  if (!TABLE.hasOwnProperty(target)) {
-    throw new Error(`no precompiled module found for ${target}`);
-  }
-  loaded = TABLE[target]();
-  return loaded;
-}
-
-module.exports = {
-  get fromFile() { return load().fromFile; },
-  get fromStdin() { return load().fromStdin; },
-  get findArtifact() { return load().findArtifact; },
-  get findFileByCrateType() { return load().findFileByCrateType; }
-};
+}, [
+  'fromFile',
+  'fromStdin',
+  'findArtifact',
+  'findFileByCrateType'
+]);
