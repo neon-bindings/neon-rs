f509d915256c16babd3fd813e2b6abbe3cee8abf
David Herman <david.herman@gmail.com>
2023-05-21 12:27:59 -0700

    See if export * was the real culprit

--- pkgs/cargo-messages/esm/index.js
+++ pkgs/cargo-messages/esm/index.js
@@ -1,1 +1,7 @@
-export * from '../cjs/index.cjs';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+
+const { CargoMessages } = require('../cjs/index.cjs');
+
+export { CargoMessages };
