abdd1b7e4b5bec0926fce7009d3170b18fa08233
David Herman <david.herman@gmail.com>
2023-07-17 17:37:40 -0700

    moment of truth: use the async CargoReader API instead of the old sync CargoMessages!

--- src/cli/src/commands/dist.ts
+++ src/cli/src/commands/dist.ts
@@ -1,7 +1,8 @@
+import { createReadStream } from 'node:fs';
 import { copyFile } from 'node:fs/promises';
 import commandLineArgs from 'command-line-args';
 import { Command, CommandDetail } from '../command.js';
-import { CargoMessages } from 'cargo-messages';
+import { CargoMessages, CargoReader } from 'cargo-messages';
 
 // FIXME: add options to infer crate name from manifests
 // --package <path/to/package.json>
@@ -15,6 +16,10 @@ const OPTIONS = [
   { name: 'out', alias: 'o', type: String, defaultValue: 'index.node' },
   { name: 'verbose', alias: 'v', type: Boolean, defaultValue: false }
 ];
+
+function createInputStream(file: string | null): NodeJS.ReadableStream {
+  return file ? createReadStream(file) : process.stdin;
+}
 
 export default class Dist implements Command {
   static summary(): string { return 'Generate a .node file from a build.'; }
@@ -72,29 +77,51 @@ export default class Dist implements Command {
     this._verbose = !!options.verbose;
   }
 
-  findArtifact(): string {
-    const messages: CargoMessages = new CargoMessages({
+  async findArtifact(): Promise<string> {
+    const reader: CargoReader = new CargoReader(createInputStream(this._log), {
       mount: this._mount || undefined,
       manifestPath: this._manifestPath || undefined,
-      file: this._log || undefined,
       verbose: this._verbose
     });
 
-    const artifact = messages.findArtifact(this._crateName);
-    if (!artifact) {
+    let file: string | null = null;
+
+    for await (const msg of reader) {
+      if (msg.isCompilerArtifact() && msg.crateName() === this._crateName) {
+        file = msg.findFileByCrateType('cdylib');
+        if (!file) {
+          throw new Error(`No artifacts were generated for crate ${this._crateName}`);
+        }
+      }
+    }
+
+    if (!file) {
       throw new Error(`No artifacts were generated for crate ${this._crateName}`);
     }
 
-    const file = artifact.findFileByCrateType('cdylib');
-    if (!file) {
-      throw new Error(`No cdylib artifact found for crate ${this._crateName}`);
-    }
-
     return file;
+    // const messages: CargoMessages = new CargoMessages({
+    //   mount: this._mount || undefined,
+    //   manifestPath: this._manifestPath || undefined,
+    //   file: this._log || undefined,
+    //   verbose: this._verbose
+    // });
+
+    // const artifact = messages.findArtifact(this._crateName);
+    // if (!artifact) {
+    //   throw new Error(`No artifacts were generated for crate ${this._crateName}`);
+    // }
+
+    // const file = artifact.findFileByCrateType('cdylib');
+    // if (!file) {
+    //   throw new Error(`No cdylib artifact found for crate ${this._crateName}`);
+    // }
+
+    // return file;
   }
 
   async run() {
-    const file = this._file || this.findArtifact();
+    const file = this._file || (await this.findArtifact());
 
     // FIXME: needs all the logic of cargo-cp-artifact (timestamp check, M1 workaround, async, errors)
     await copyFile(file, this._out);
