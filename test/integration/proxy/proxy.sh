c857ffa22f350152b763c84eb8ae3b97aa4e5e6e
David Herman <david.herman@gmail.com>
2023-09-10 15:23:39 -0700

    move proxy script into proxy.sh

--- test/integration/proxy/proxy.sh
+++ test/integration/proxy/proxy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-nohup npx verdaccio --config ./config.yml &
+# Boot the server in a background process.
+nohup npx verdaccio --config ./config.yml --listen ${{ env.PROXY }} &
+
+# Wait for the server to begin listening for connections
+( tail -F -n10 proxy.log & ) | fgrep -q 'http://127.0.0.1:4873/'
+
+cat proxy.log
