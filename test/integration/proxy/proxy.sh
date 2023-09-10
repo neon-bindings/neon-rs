0ade96b158635eb631add26f80bbdc26f7a68ba4
David Herman <david.herman@gmail.com>
2023-09-10 15:29:25 -0700

    pass ${{ env.PROXY }} correctly into proxy.sh as a command-line arg

--- test/integration/proxy/proxy.sh
+++ test/integration/proxy/proxy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+PROXY=$1
+
 # Boot the server in a background process.
-nohup npx verdaccio --config ./config.yml --listen ${{ env.PROXY }} &
+nohup npx verdaccio --config ./config.yml --listen $PROXY &
 
 # Wait for the server to begin listening for connections
-( tail -F -n10 proxy.log & ) | fgrep -q 'http://127.0.0.1:4873/'
+( tail -F -n10 proxy.log & ) | fgrep -q $PROXY
 
 cat proxy.log
