1182e1ef44c251b8577e50497e245fc5a3d62ab4
David Herman <david.herman@gmail.com>
2023-09-10 23:14:35 -0700

    debugging

--- test/integration/sniff-bytes/lib/load.cjs
+++ test/integration/sniff-bytes/lib/load.cjs
@@ -1,6 +1,7 @@
 module.exports = require('@neon-rs/load').lazy({
   targets: {
-    'linux-x64-gnu': () => require('@sniff-bytes/linux-x64-gnu')
+    'linux-x64-gnu': () => require('@sniff-bytes/linux-x64-gnu'),
+    'darwin-arm64': () => require('@sniff-bytes/darwin-arm64')
   },
   exports: [
     'sniffBytes'
