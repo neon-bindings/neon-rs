a8fc5cf0554afa1d460d79f66c1044cad245a126
David Herman <david.herman@gmail.com>
2023-09-10 23:02:36 -0700

    load fixtures

--- test/integration/test-sniff-bytes/src/main.test.ts
+++ test/integration/test-sniff-bytes/src/main.test.ts
@@ -1,7 +1,19 @@
+import { readFile } from 'node:fs/promises';
+import * as path from 'node:path';
 import { describe, expect, test } from '@jest/globals';
+import { sniffBytes } from 'sniff-bytes';
+
+async function loadFixture(name: string) {
+  return (await readFile(path.join('.', 'fixtures', name))).buffer;
+}
 
 describe('main', () => {
   test('first test', () => {
     expect(1 + 2).toBe(3);
+  });
+
+  test('load a fixture', async () => {
+    const jpg = await loadFixture('pit-droids.jpg');
+    console.error(jpg);
   });
 });
