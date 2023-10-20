51e7ed5ca8058575a090c0f526f26918d33cccf4
David Herman <dherman@linkedin.com>
2023-10-20 10:21:00 -0700

    try verdaccio 6 pre-release to see if the bug is fixed

--- test/integration/proxy/proxy.sh
+++ test/integration/proxy/proxy.sh
@@ -3,7 +3,7 @@
 PROXY=$1
 
 # Boot the server in a background process.
-nohup npx verdaccio --config ./config.yml --listen $PROXY &
+nohup npx verdaccio@6.0.0-6-next.76 --config ./config.yml --listen $PROXY &
 
 # Wait for the server to begin listening for connections
 ( tail -F -n10 proxy.log & ) | fgrep -q $PROXY
