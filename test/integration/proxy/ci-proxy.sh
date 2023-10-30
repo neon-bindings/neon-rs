78d43c2625ccd23d07b9833a00149937e6acefef
David Herman <dherman@linkedin.com>
2023-10-30 00:32:13 -0700

    get rid of all the duplicated configuration values in the CI workflow since they're already in the shell scripts

--- test/integration/proxy/ci-proxy.sh
+++ test/integration/proxy/ci-proxy.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-CIPROXY=$1
-
-if [ $# -ne 1 ]; then
-  echo -e 'usage: proxy.sh <url>[:<port>]'
-  echo -e '  <url>   registry server URL'
-  echo -e '  <port>  optional registry server port (default: 80)'
-  echo -e
-  echo -e 'error: <url> not specified'
-  exit 1
-fi
+CIPROXY=http://127.0.0.1:4873
 
 # Boot the server in a background process.
 nohup npx verdaccio --config ./config.yml --listen $CIPROXY &
--- test/integration/proxy/local-proxy.sh
+++ test/integration/proxy/local-proxy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd $(dirname $0)/../../..
+ROOT_DIR=$(pwd)
+PROXY_DIR=${ROOT_DIR}/test/integration/proxy
+LOCAL_PROXY=http://127.0.0.1:4873
+
+cd ${PROXY_DIR}
+
 if ! which pm2 >/dev/null ; then
   echo -e 'usage: local-proxy.sh'
   echo -e
@@ -21,15 +28,16 @@ if pm2 describe neon-test-proxy >/dev/null 2>&1 ; then
   exit 1
 fi
 
-rm -rf storage proxy.log
+rm -rf ./storage ./proxy.log
 
 # Boot the server in a background process.
-pm2 start verdaccio --name neon-test-proxy --no-autorestart -- --config ./config.yml --listen http://127.0.0.1:4873
+pm2 start verdaccio --name neon-test-proxy --no-autorestart -- --config ./config.yml --listen ${LOCAL_PROXY}
 
 # Wait for the server to begin listening for connections
-( tail -F -n10 proxy.log & ) | fgrep -q http://127.0.0.1:4873
+# "${LOCAL_PROXY}"
+( tail -F -n10 ./proxy.log & ) | fgrep -q "${LOCAL_PROXY}"
 
-cat proxy.log
+cat ./proxy.log
 
 echo
 echo 'Proxy `neon-test-proxy` started. You can now control it using pm2:'
