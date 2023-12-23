b1010e829f97a98cbd2f23d2ccc98de10ed31b85
David Herman <david.herman@gmail.com>
2023-12-22 21:26:37 -0800

    Upgrade to use neon-actions/build@v0.6: - Easier-to-understand YAML API: `platform` is all you need, no more `input-directory` - `matrix.target` becomes `matrix.platform` - Eliminate all uses of `neon tarball` command and use `npm pack` directly

--- test/integration/proxy/publish.sh
+++ test/integration/proxy/publish.sh
@@ -4,6 +4,17 @@ PROXY_DIR=$(dirname $0)
 cd ${PROXY_DIR}/../../..
 ROOT_DIR=$(pwd)
 
+if [ $# -lt 1 ]; then
+  echo -e 'usage: publish.sh <platform>'
+  echo -e ''
+  echo -e '  <platform>  Node platform name'
+  echo -e ''
+  echo -e 'error: missing <platform> argument'
+  exit 1
+fi
+
+CURRENT_PLATFORM=$1
+
 PROXY_USER=ci
 PROXY_PASSWORD=dummycipassword
 PROXY_EMAIL=ci@neon-bindings.com
@@ -17,6 +28,8 @@ cd test/integration/sniff-bytes
 npm i
 npm run build
 mkdir -p dist
-PACKAGE_TARBALL=$(npx neon tarball --out-dir dist | tail -1)
-npm publish ./${PACKAGE_TARBALL} --registry $PROXY_SERVER
+# NOTE: `xargs basename` is a workaround for https://github.com/npm/cli/issues/3405
+PACKAGE_TARBALL=$((cd platforms/$CURRENT_PLATFORM && npm pack --json | jq '.[0].filename' | xargs basename))
+mv ./platforms/${CURRENT_PLATFORM}/${PACKAGE_TARBALL} ./dist/
+npm publish ./dist/${PACKAGE_TARBALL} --registry $PROXY_SERVER
 npm publish --registry $PROXY_SERVER
