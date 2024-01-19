957dee47e4d97b9a3d700319298bc63d6d4cdb37
David Herman <david.herman@gmail.com>
2024-01-19 15:55:03 -0800

    - trivial code cleanup in src/install - experiment with removing the global.require fixup for src/cli

--- src/install/src/index.ts
+++ src/install/src/index.ts
@@ -5,16 +5,18 @@ import { execa } from 'execa';
 // Inherit the install command (`install` vs `ci`) from the current running npm command (if any).
 const cmd = process.env.npm_command === 'ci' ? 'ci' : 'install';
 const args = process.argv;
+const cwd = args[2];
+const rest = args.slice(3);
 
 console.error(`[install] $npm_command=${process.env.npm_command}`);
-console.error(`[install] running in working directory: ${args[2]}`)
-console.error(`[install] running command: npm ${cmd} ${args.slice(3).join(' ')}`);
+console.error(`[install] running in working directory: ${cwd}`)
+console.error(`[install] running command: npm ${cmd} ${rest.join(' ')}`);
 
-const result = await execa('npm', [cmd].concat(args.slice(3)), {
+const result = await execa('npm', [cmd].concat(rest), {
   shell: true,
   stdout: 'inherit',
   stderr: 'inherit',
-  cwd: args[2]
+  cwd
 });
 
 process.exit(result.exitCode);
