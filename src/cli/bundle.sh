2c4d5c626c0af44d9895ac5e78544240157d9457
David Herman <david.herman@gmail.com>
2024-01-28 14:55:10 -0800

    hide the spewing of the bundle contents behind an env var

--- src/cli/bundle.sh
+++ src/cli/bundle.sh
@@ -3,9 +3,11 @@
 ncc build src/index.ts -o ../../dist/cli
 crlf --set=LF ../../dist/cli/index.js
 
-echo === BEGIN BUNDLE ===
-cat ../../dist/cli/index.js
-echo === END BUNDLE ===
+if [[ $DEBUG_BUNDLE != '' ]]; then
+  echo === BEGIN BUNDLE ===
+  cat ../../dist/cli/index.js
+  echo === END BUNDLE ===
+fi
 
 cp README.md LICENSE* ../../dist/cli
 cat package.json | jq -f bundle.jq > ../../dist/cli/package.json
