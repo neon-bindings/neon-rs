dbcc41dc9c594b4a153216bb69c9332255741320
David Herman <david.herman@gmail.com>
2024-01-28 15:03:09 -0800

    more hiding spew behind an env var

--- src/install/bundle.sh
+++ src/install/bundle.sh
@@ -3,8 +3,10 @@
 ncc build src/index.ts -o ../../dist/install
 crlf --set=LF ../../dist/install/index.js
 
-echo === BEGIN BUNDLE ===
-cat ../../dist/install/index.js
-echo === END BUNDLE ===
+if [[ $DEBUG_BUNDLE != '' ]]; then
+  echo === BEGIN BUNDLE ===
+  cat ../../dist/install/index.js
+  echo === END BUNDLE ===
+fi
 
 cat package.json | jq -f bundle.jq > ../../dist/install/package.json
