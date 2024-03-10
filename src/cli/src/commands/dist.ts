c6433fc213ff5c23e6aff4d8ed7ad4ace7ad058a
David Herman <david.herman@gmail.com>
2024-03-09 18:55:41 -0800

    add support for `$NEON_BUILD_PLATFORM` and `--platform` to `neon dist`, and deprecate `-o`

--- src/cli/src/commands/dist.ts
+++ src/cli/src/commands/dist.ts
@@ -3,6 +3,8 @@ import { copyFile } from 'node:fs/promises';
 import commandLineArgs from 'command-line-args';
 import { Command, CommandDetail, CommandSection } from '../command.js';
 import { CargoMessages, CargoReader } from 'cargo-messages';
+import { LibraryManifest } from '@neon-rs/manifest';
+import { assertIsNodePlatform } from '@neon-rs/manifest/platform';
 
 // FIXME: add options to infer crate name from manifests
 // --package <path/to/package.json>
@@ -13,7 +15,9 @@ const OPTIONS = [
   { name: 'log', alias: 'l', type: String, defaultValue: null },
   { name: 'mount', alias: 'm', type: String, defaultValue: null },
   { name: 'manifest-path', type: String, defaultValue: null },
-  { name: 'out', alias: 'o', type: String, defaultValue: null },
+  { name: 'out', alias: 'o', type: String, defaultValue: null }, // DEPRECATED: 0.2
+  { name: 'platform', alias: 'p', type: String, defaultValue: null },
+  { name: 'debug', alias: 'd', type: Boolean, defaultValue: false },
   { name: 'verbose', alias: 'v', type: Boolean, defaultValue: false }
 ];
 
@@ -32,20 +36,52 @@ function ensureDefined(str: string | undefined, msg: string): string {
   return str;
 }
 
+function parseOutputFile(debug: boolean, out: string | undefined, platform: string | undefined): Promise<string> {
+  if (debug && out) {
+    throw new Error("Options --debug and --out cannot both be enabled.");
+  } else if (debug && platform) {
+    throw new Error("Options --debug and --platform cannot both be enabled.");
+  } else if (out && platform) {
+    throw new Error("Options --out and --platform cannot both be enabled.");
+  }
+
+  const NEON_DIST_OUTPUT = process.env['NEON_DIST_OUTPUT'];
+  const NEON_BUILD_PLATFORM = process.env['NEON_BUILD_PLATFORM'];
+
+  if (platform || (!debug && NEON_BUILD_PLATFORM)) {
+    const p = platform || NEON_BUILD_PLATFORM;
+    assertIsNodePlatform(p);
+    return LibraryManifest.load().then(manifest => {
+      const path = manifest.getPlatformOutputPath(p);
+      if (!path) {
+        throw new Error(`Platform $p not supported by this library.`);
+      }
+      return path;
+    });
+  } else if (out || (!debug && NEON_DIST_OUTPUT)) {
+    const path = out || NEON_DIST_OUTPUT;
+    return Promise.resolve(path!);
+  } else {
+    return Promise.resolve('index.node');
+  }
+}
+
 export default class Dist implements Command {
   static summary(): string { return 'Generate a binary .node file from a cargo output log.'; }
-  static syntax(): string { return 'neon dist [-n <name>] [-f <dylib>|[-l <log>] [-m <path>]] [-o <dist>]'; }
+  static syntax(): string { return 'neon dist [-n <name>] [-f <file>|[-l <log>] [-m <path>]] [-p <platform> | -d]'; }
   static options(): CommandDetail[] {
     return [
       { name: '-n, --name', summary: 'Crate name. (Default: $npm_package_name)' },
-      { name: '-f, --file <dylib>', summary: 'Build .node from dylib file <dylib>.' },
+      { name: '-f, --file <file>', summary: 'Build .node from dylib file <file>.' },
       { name: '-l, --log <log>', summary: 'Find dylib path from cargo messages <log>. (Default: stdin)' },
       {
         name: '-m, --mount <path>',
         summary: 'Mounted path of target directory in virtual filesystem. This is used to map paths from the log data back to their real paths, needed when tools such as cross-rs report messages from within a mounted Docker filesystem.'
       },
       { name: '--manifest-path <path>', summary: 'Real path to Cargo.toml. (Default: cargo behavior)' },
-      { name: '-o, --out <dist>', summary: 'Copy output to file <dist>. (Default: $NEON_DIST_OUTPUT or index.node)' },
+      // { name: '-o, --out <dist>', summary: 'Copy output to file <dist>. (Default: $NEON_DIST_OUTPUT or index.node)' },
+      { name: '-p, --platform <platform>', summary: 'Stage output file for caching to platform <platform>. (Default: $NEON_BUILD_PLATFORM or -d)' },
+      { name: '-d, --debug', summary: 'Generate output file for debugging (./index.node)' },
       { name: '-v, --verbose', summary: 'Enable verbose logging. (Default: false)' }
     ];
   }
@@ -62,7 +98,7 @@ export default class Dist implements Command {
   private _mount: string | null;
   private _manifestPath: string | null;
   private _crateName: string;
-  private _out: string;
+  private _out: Promise<string>;
   private _verbose: boolean;
 
   constructor(argv: string[]) {
@@ -86,9 +122,7 @@ export default class Dist implements Command {
     this._manifestPath = options['manifest-path'];
     this._crateName = options.name ||
       basename(ensureDefined(process.env['npm_package_name'], '$npm_package_name'));
-    this._out = options.out ||
-      process.env['NEON_DIST_OUTPUT'] ||
-      'index.node';
+    this._out = parseOutputFile(options.debug, options.out, options.platform);
     this._verbose = !!options.verbose;
 
     this.log(`crate name = "${this._crateName}"`);
@@ -147,6 +181,6 @@ export default class Dist implements Command {
     const file = this._file || (await this.findArtifact());
 
     // FIXME: needs all the logic of cargo-cp-artifact (timestamp check, M1 workaround, async, errors)
-    await copyFile(file, this._out);
+    await copyFile(file, await this._out);
   }
 }
