7062843f2951d4502162c21f3da683b60a632a6e
David Herman <david.herman@gmail.com>
2024-03-23 12:36:24 -0700

    npm run dist && npm i

--- dist/cli/index.js
+++ dist/cli/index.js
@@ -40439,7 +40439,7 @@ class Dist {
     async run() {
         const file = this._file || (await this.findArtifact());
         const out = await this._out;
-        this.log(`output file option: ${out.option}`);
+        this.log(`output option = ${out.option}`);
         this.log(`output file = ${out.path}`);
         // FIXME: needs all the logic of cargo-cp-artifact (timestamp check, M1 workaround, async, errors)
         await (0,promises_.copyFile)(file, out.path);
