aad99a1f8a87efdba0e20d67e5022668682aa870
David Herman <david.herman@gmail.com>
2024-03-23 23:45:34 -0700

    --no-workspaces-update

--- src/bump-workspaces.sh
+++ src/bump-workspaces.sh
@@ -4,10 +4,10 @@ bash ../check-src-pollution.sh
 
 # npm version --workspaces patch
 
-(cd cli && npm version patch)
+(cd cli && npm version patch --no-workspaces-update)
 
 bash ../check-src-pollution.sh
 
-(cd install && npm version patch)
+(cd install && npm version patch --no-workspaces-update)
 
 bash ../check-src-pollution.sh
