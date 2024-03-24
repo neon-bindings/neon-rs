e7ea4984d06408941ce4503bd83ca31f3496ae1c
David Herman <david.herman@gmail.com>
2024-03-24 12:28:27 -0700

    let's work on update-cargo-messages.sh...

--- src/cli/update-cargo-messages.sh
+++ src/cli/update-cargo-messages.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 cargo_messages_version=$(npm view cargo-messages version)
-read -a platforms <<< $(npm ls --json | jq -r '.dependencies."@neon-rs/cli".dependencies | keys | map(select(startswith("@cargo-messages"))) | map(sub("@cargo-messages/"; "")) | join("\n")')
+
+read -a platforms <<< $( (cd ../../pkgs/cargo-messages && node ../../dist/cli list-platforms) | jq -r 'keys | join("\n")' )
 
 for platform in ${platforms[@]} ; do
+  echo "npm i --omit=optional -E -O @cargo-messages/$platform@$cargo_messages_version"
   npm i --omit=optional -E -O @cargo-messages/$platform@$cargo_messages_version
 done
 
+echo "npm i --omit=optional cargo-messages@$cargo_messages_version"
 npm i --omit=optional cargo-messages@$cargo_messages_version
