e75cff5ce9c2f9c17f802672b48ce6edae9ed865
David Herman <david.herman@gmail.com>
2024-03-23 23:30:45 -0700

    more diagnostics

--- src/bump-workspaces.sh
+++ src/bump-workspaces.sh
@@ -2,6 +2,12 @@
 
 bash ../check-src-pollution.sh
 
-npm version --workspaces patch
+# npm version --workspaces patch
+
+(cd cli && npm version patch)
+
+bash ../check-src-pollution.sh
+
+(cd install && npm version patch)
 
 bash ../check-src-pollution.sh
