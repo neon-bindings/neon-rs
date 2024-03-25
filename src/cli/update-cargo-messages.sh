d4a616c11feea3837c0edf7569d070a08300f4e7
David Herman <david.herman@gmail.com>
2024-03-24 17:41:03 -0700

    more diagnostics

--- src/cli/update-cargo-messages.sh
+++ src/cli/update-cargo-messages.sh
@@ -4,10 +4,12 @@ cargo_messages_version=$(npm view cargo-messages version)
 
 read -a platforms <<< $( (cd ../../pkgs/cargo-messages && node ../../dist/cli list-platforms) | jq -r 'keys | join("\n")' )
 
+echo "[update-cargo-messages.sh] platforms: ${platforms[@]}"
+
 for platform in ${platforms[@]} ; do
-  echo "npm i --omit=optional -E -O @cargo-messages/$platform@$cargo_messages_version"
-  npm i --omit=optional -E -O @cargo-messages/$platform@$cargo_messages_version
+  echo "[update-cargo-messages.sh] npm i --omit=optional -E -O @cargo-messages/$platform@$cargo_messages_version"
+  npm i --omit=optional -E -O @cargo-messages/$platform@$cargo_messages_version || echo "❌ failed to update @cargo-messages/$platform"
 done
 
-echo "npm i --omit=optional cargo-messages@$cargo_messages_version"
-npm i --omit=optional cargo-messages@$cargo_messages_version
+echo "[update-cargo-messages.sh] npm i --omit=optional cargo-messages@$cargo_messages_version"
+npm i --omit=optional cargo-messages@$cargo_messages_version || echo "❌ failed to update cargo-messages"
