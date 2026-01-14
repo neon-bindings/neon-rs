7c90bbd00c9e67091fa83e92ce95a7d7b3244f3e
David Herman <david.herman@gmail.com>
2026-01-13 16:38:29 -0800

    re-bundle the CLI with the updated cargo-messages

--- test/integration/proxy/publish.sh
+++ test/integration/proxy/publish.sh
@@ -41,6 +41,17 @@ echo "*********************************"
 (cd pkgs/cargo-messages/platforms/linux-x64-gnu && npm publish --registry $PROXY_SERVER)
 latest_version=$(npm view --registry $PROXY_SERVER cargo-messages version)
 (cd pkgs/load && npm publish --registry $PROXY_SERVER)
+
+echo "*********************************"
+echo REBUILDING AND PUBLISHING CLI TO NPM PROXY
+echo "*********************************"
+
+(cd src/cli && npm install "cargo-messages@${latest_version}" && (NPM_CONFIG_REGISTRY=$PROXY_SERVER npm run dist))
+
+echo "+++++++++++++++++++++++++++++++++"
+cat dist/cli/index.js
+echo "+++++++++++++++++++++++++++++++++"
+
 (cd dist/cli && npm update --registry $PROXY_SERVER cargo-messages && npm install -O --registry $PROXY_SERVER "@cargo-messages/linux-x64-gnu@$latest_version" && npm publish --registry $PROXY_SERVER)
 
 echo "*********************************"
