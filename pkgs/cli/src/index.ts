8cc01c642c8e30f31c09bbc6258819f863e404eb
David Herman <david.herman@gmail.com>
2026-03-09 13:04:45 -0700

    delete require shim hack that was only needed for ncc to be able to link to binary modules

--- pkgs/cli/src/index.ts
+++ pkgs/cli/src/index.ts
@@ -4,20 +4,6 @@ import commandLineCommands from 'command-line-commands';
 import { printErrorWithUsage, printError, printMainUsage } from './print.js';
 import { Command, CommandName, CommandClass, asCommandName, commandFor } from './command.js';
 
-import { createRequire } from 'node:module';
-
-const absoluteRequire = createRequire(import.meta.url);
-
-// When compiling with ncc, the default global `require` function does
-// not know how to find the binary `@cargo-messages/*` modules, but the
-// compiled result of `createRequire` does. So this replaces the global
-// `require` with that one. This way when the `cargo-messages` loader
-// module attempts to load the right binary module for the device, the
-// call to `require` succeeds.
-global['require'] = function(spec: string) {
-  return absoluteRequire(spec);
-} as any;
-
 class Cli {
   parse(): Command {
     try {
